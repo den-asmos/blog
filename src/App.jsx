c9e39c3fecfa2b08404fe597ec7fb4b9e89d5fb9
Eva <den_asmos@mail.ru>
2023-08-24 08:08:36 +0300

    Create main blog component with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
 import styled from 'styled-components';
+import { Routes, Route } from 'react-router-dom';
 
-const Div = styled.div`
-	text-align: center;
-	color: blue;
+const Content = styled.div`
+	padding: 120px 0;
 `;
+
+const H2 = styled.h2`
+	text-align: center;
+`;
+
+const Header = () => {
+	return <div>Header</div>;
+};
+
+const Footer = () => {
+	return <div>Footer</div>;
+};
 
 const App = () => {
 	return (
-		<Div>
-			<i className="fa fa-camera-retro"></i>
-			<div>Hello</div>
-		</Div>
+		<>
+			<Header />
+			<Content>
+				<H2>Page Content</H2>
+				<Routes>
+					<Route path="/" element={<div>Main Page</div>} />
+					<Route path="/login" element={<div>Auth Page</div>} />
+					<Route path="/register" element={<div>Register Page</div>} />
+					<Route path="/users" element={<div>Users Page</div>} />
+					<Route path="/post/:postId" element={<div>Post Page</div>} />
+					<Route path="/post" element={<div>New Post Page</div>} />
+					<Route path="*" element={<div>Error Page</div>} />
+				</Routes>
+			</Content>
+			<Footer />
+		</>
 	);
 };
 
